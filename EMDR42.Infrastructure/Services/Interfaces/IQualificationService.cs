f2f20f423d3d96f339b03fd3ff410a534e98a4e6
Denis <ponomarevd61@gmail.com>
2025-02-07 23:16:29 +0300

    edit services:

---
+++ EMDR42.Infrastructure/Services/Interfaces/IQualificationService.cs
@@ -0,0 +1,13 @@
+﻿using EMDR42.Domain.Commons.DTO;
+using EMDR42.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMDR42.Infrastructure.Services.Interfaces;
+
+public interface IQualificationService : IAsyncRepository<QualificationDTO, QualificationModel>
+{
+}
